9fca311a350a970b97a0bb6c2ea248292c23f857
lkb <tmwlak@gmail.com>
2023-05-10 16:07:28 +0900

    -

---
+++ src/IAmPromise.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ArrayKeyTest from './ArrayKeyTest'
+import './App.css'
+
+export async function IAmPromise() {
+  
+  return (
+    <>
+      <div className="">i am async component</div>
+    </>
+  )
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
+
+// import {UseHookTest} from './UseHookTestBefore.tsx'
+import React, { Suspense } from 'react'
 import App from './App.tsx'
 import AppComponentIsNeverUnmounted from './AppComponentIsNeverUnmounted.tsx'
 import {FetchExample} from './FetchExample.tsx'
 import ReRenderTest from './ReRenderTest.tsx'
 import {AppForContext} from './ContextTest.tsx'
-// import {UseHookTest} from './UseHookTestBefore.tsx'
 import {UseHookTest} from './UseHookTestAfter.tsx'
 import {LazyCallTest} from './LazyCallTest.tsx'
+
 import './index.css'
+import { IAmPromise } from './IAmPromise.tsx'
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -19,8 +22,15 @@ ReactDOM.createRoot(
     // <Suspense fallback={"loading..."}>
       // <UseHookTest url={"https://jsonplaceholder.typicode.com/todos/1"} shouldFetch />
     // </Suspense>
-    <LazyCallTest />
-
+    // <LazyCallTest />
+    // <div className="" style={{border : "1px solid red"}}>
+    //     <div className="">i am parent component</div>
+    // </div>
+    <div className="" style={{border : "1px solid red"}}>
+          <div className="">i am parent component</div>
+          {/* @ts-expect-error Async Server Component */}
+          <IAmPromise/>      
+    </div>
 )
 
 // ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
